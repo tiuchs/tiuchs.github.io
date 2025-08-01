d6bfd31d211cdc04791f0960eb3ad1b9b5fb1063
tiuchs <tiuchs@gmail.com>
2025-07-31 22:42:23 -0500

    new project

---
+++ ping/sw.js
@@ -0,0 +1,41 @@
+// sw.js
+const CACHE_NAME = "netstatus-cache-v1";
+const urlsToCache = [
+  "./",
+  "./logo.png",
+  "./manifest.json"
+];
+
+// Install SW and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Fetch from cache, fallback to network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// (Optional) Activate new SW and remove old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
